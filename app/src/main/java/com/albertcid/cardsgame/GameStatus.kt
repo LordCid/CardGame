b4d897d9e531e151278a12902ac904c437ed2315
ALBERTPORTATIL\Albert <cidalbert@hotmail.com>
2020-10-13 10:34:16 +0200

    typo fix

--- app/src/main/java/com/albertcid/cardsgame/GameStatus.kt
+++ app/src/main/java/com/albertcid/cardsgame/GameStatus.kt
@@ -1,11 +1,11 @@
 package com.albertcid.cardsgame
 
 data class GameStatus(
-    val currentRound: Int,
-    val isUserWinnerOfRound: Boolean,
-    val userCardPlayed: String,
-    val OpponentCardPlayed: String,
-    val totalUsersCardPile: Int,
-    val totalUsersDiscardPile: Int,
-    val totalOpponentDiscardPile: Int
+        val currentRound: Int,
+        val isUserWinnerOfRound: Boolean,
+        val userCardPlayed: String,
+        val opponentCardPlayed: String,
+        val totalUsersCardPile: Int,
+        val totalUsersDiscardPile: Int,
+        val totalOpponentDiscardPile: Int
 )
